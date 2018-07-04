097152ed38c227f174f2ba6e5bd520ff4935224e
Kiran <enginerkiru@gmail.com>
2018-07-05 01:52:50 +0545

    Functional Neptube

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
 import YTsearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+import './styles/style.css';
 
 const API_KEY='AIzaSyBqH5x6jpGxs5NXqXNcWaLeuzarDf1zpXw';
 
 class App extends Component {
   constructor(props){
   super(props);
-  this.state ={ videos:[]};
-  
-YTsearch({key:API_KEY,term:'Nepal'},(videos)=>{
-  this.setState({videos});
-});
-
+  this.state ={ 
+    videos:[],
+    selectedVideo:null,
+   
+  };
+  this.videoSearch('NarayanGopalSongs');
+}
+  videoSearch(term){
+        YTsearch({key:API_KEY,term:term},(videos)=>{
+        this.setState({videos:videos,
+        selectedVideo: videos[0]
+    });
+    });
   }
   render() {
     return (
       <div>
-      <SearchBar/>
-      <VideoList videos={this.state.videos}/>
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList 
+      videos={this.state.videos}
+      onVideoSelect={selectedVideo=> this.setState({selectedVideo})}/>
       </div>
     );
   }
--- client/src/components/video_list_item.js
+++ client/src/components/video_list_item.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) =>
+const VideoListItem = ({video,onVideoSelect}) =>
 {
-    return <li>Video</li>
+    const imageURL= video.snippet.thumbnails.default.url;
+    return( 
+        <li onClick={()=> onVideoSelect(video)}  className="list-group-item">
+        <div className="video-list media">
+        <div className="media-left">
+            <img className="media-object" alt="Youtube" src={imageURL}/>
 
+        </div>
+        <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+        </div>
+        </div>
+
+        </li>
+
+
+    );
 };
+
 export default VideoListItem;
