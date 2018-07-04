0ca80202a747bb6906738de9b1ffbe0cd2a13cbc
Kiran <enginerkiru@gmail.com>
2018-07-04 12:33:19 +0545

    added videolist

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
 import YTsearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY='AIzaSyBqH5x6jpGxs5NXqXNcWaLeuzarDf1zpXw';
 
@@ -18,6 +19,7 @@ YTsearch({key:API_KEY,term:'Nepal'},(videos)=>{
     return (
       <div>
       <SearchBar/>
+      <VideoList videos={this.state.videos}/>
       </div>
     );
   }
