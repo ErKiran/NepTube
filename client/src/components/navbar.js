4a65313ee615a9dbe0b8ed47393e7b9be1c35e15
Kiran <enginerkiru@gmail.com>
2018-07-08 12:24:17 +0545

    Build Version of React is created

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,10 +8,10 @@ export default class SearchBar extends Component {
     render(){
     return(
       <nav className="navbar navbar-light bg-light justify-content-between">
-<a className="navbar-brand">Munutube</a>
+<a className="navbar-brand">Neptube</a>
 <form className="form-inline">
   <input className="form-control mr-sm-8" type="search" value={this.state.term}
-  onChange={event => this.onInputChange(event.target.value)} placeholder="Munu! Search gara" aria-label="Search"/>
+  onChange={event => this.onInputChange(event.target.value)} placeholder="Search Here....." aria-label="Search"/>
   <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 </form>
 </nav>
